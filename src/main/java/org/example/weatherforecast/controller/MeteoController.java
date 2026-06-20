a7e466acb6b20371c3f31914d33e191754b6afef
Aureliano <aurelianopeixoto2017@gmail.com>
2026-06-20 12:41:36 -0300

    add: resources/static to gitignore

--- src/main/java/org/example/weatherforecast/controller/MeteoController.java
+++ src/main/java/org/example/weatherforecast/controller/MeteoController.java
@@ -4,12 +4,14 @@ import org.example.weatherforecast.client.MeteoService;
 import org.example.weatherforecast.model.WeatherResponse;
 import org.example.weatherforecast.model.open_meteo_api.ResultsWeather;
 import org.example.weatherforecast.service.WeatherService;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 public class MeteoController {
     @GetMapping("/weather/raw")
--- src/main/java/org/example/weatherforecast/model/WeatherResponse.java
+++ src/main/java/org/example/weatherforecast/model/WeatherResponse.java
@@ -1,22 +1,25 @@
 package org.example.weatherforecast.model;
 
+import org.example.weatherforecast.client.GeoService;
 import org.example.weatherforecast.client.MeteoService;
 import org.example.weatherforecast.model.open_meteo_api.ResultsWeather;
+
+import java.io.IOException;
 
 public class WeatherResponse {
     private String city;
     private double temperature;
     private double actualRain;
-    private double rainProbability;
+    private float rainProbability;
 
-    public WeatherResponse(String city, ResultsWeather results) {
-        this.city = city;
+    public WeatherResponse(String city, ResultsWeather results) throws IOException {
+        this.city = new GeoService().getCoordinates(city).getName();
         this.temperature = results.getCurrent().getTemperature_2m();
         this.actualRain = results.getCurrent().getRain();
-        this.rainProbability = results.getHourly().getRainProbability().stream()
+        this.rainProbability = (float) Math.round(results.getHourly().getRainProbability().stream()
                 .mapToInt(Integer::intValue)
                 .average()
-                .orElse(0.0);
+                .orElse(0.0) * 100.0) / 100.0f;
     }
 
     public String getCity() {
@@ -31,7 +34,7 @@ public class WeatherResponse {
         return actualRain;
     }
 
-    public double getRainProbability() {
+    public float getRainProbability() {
         return rainProbability;
     }
 }
